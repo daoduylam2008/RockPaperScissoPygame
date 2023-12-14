c4d573e259896ff083756c30df5783b5daf300b8
daoduylam2008 <96049665+daoduylam2008@users.noreply.github.com>
2023-12-14 18:36:33 +0700

    summary

---
+++ main.py
@@ -0,0 +1,60 @@
+"""
+Rock Paper Scissor Game GUI Application
+
+The program is used some of the libraries which are installed from PyPI (Python Package Index)
+
+Developed by DAO DUY LAM, PHAM MINH KHOI, LE CONG TIEN
+"""
+# Python Package Index
+import pygame
+
+# System Module and Libaries
+import sys
+import uix
+
+# Autorize information, gmail
+__author1__ = "daoduylam2020@gmail.com"  # DAO DUY LAM
+__author2__ = ""  # PHAM MINH KHOI
+__author3__ = ""  # LE CONG TIEN
+
+# ___ MAIN ___
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+
+class MenuView:
+    def __init__(self, surface):
+        self.surface = surface
+
+
+class RockPaperScissor:
+    def __init__(self):
+        self.width = 900
+        self.height = 700
+        self.screen = pygame.display.set_mode((self.width, self.height))
+
+    def run(self):
+        while True:
+            # Item
+            # Event
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.closeProgram()
+            self.screen.fill(BLACK)
+
+            pygame.display.flip()
+            pygame.display.update()
+
+    def closeProgram(self):
+        pygame.quit()
+        sys.exit()
+
+
+if __name__ == "__main__":
+    game = RockPaperScissor()
+    try:
+        game.run()
+    except Exception as bug:
+        print(bug)
+        game.closeProgram()
--- uix.py
+++ uix.py
@@ -1,5 +1,28 @@
 import pygame
 
+
 class Button:
-    def __init__(self):
-        pass
+    def __init__(self,
+                 surface,
+                 rect: tuple,
+                 mouse_pos: tuple,
+                 event,
+                 text="",
+                 color=(180, 180, 180),
+                 press_color=(100, 100, 100),
+                 on_touch_color=(150, 150, 150),
+                 ):
+        self.surface = surface
+        self.rect = pygame.Rect(rect[0], rect[1], rect[2], rect[3])
+        self.mouse_pos = mouse_pos
+        self.event = event
+        self.text = text
+        self.color = color
+        self.press_color = press_color
+        self.on_touch_color = on_touch_color
+
+    def on_touch(self): pass
+
+    def on_press(self): pass
+
+    def on_release(self): pass
