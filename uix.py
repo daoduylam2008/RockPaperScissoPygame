a9075e5ea34d3ade6b3cc7b645c736e28bece9f4
daoduylam2008 <96049665+daoduylam2008@users.noreply.github.com>
2023-12-14 19:31:46 +0700

    Update uix.py

--- uix.py
+++ uix.py
@@ -11,34 +11,42 @@ class Button:
                  color=(180, 180, 180),
                  press_color=(100, 100, 100),
                  on_touch_color=(150, 150, 150),
-                 text_color=(0, 0, 0)
+                 text_color=(0, 0, 0),
+                 on_press_action=None,
+                 on_release_action=None,
+                 on_touch_action=None
                  ):
         self.surface = surface
         self.rect = pygame.Rect(rect[0], rect[1], rect[2], rect[3])
         self.mouse_pos = mouse_pos
+
         self.event = event
+
         self.text = self._textForButton(text)
+
         self.color = color
         self.press_color = press_color
         self.on_touch_color = on_touch_color
         self.text_color = text_color
 
+        self.on_touch_action = on_touch_action
+
     def create(self):
         pygame.draw.rect(self.surface, self.color)
-
-    def on_touch(self, event) -> bool:
-        self.event = event
-        if self.rect.collidepoint(self.mouse_pos):
-            return True
-        else:
-            return False
 
     def _textForButton(self, text):
         font = pygame.font.Font("times new roman", 15)
         txt = font.render(text, True, self.text_color)
         return txt
 
-    def on_press(self, event, button) -> bool:
+    def getTouch(self, event) -> bool:
+        self.event = event
+        if self.rect.collidepoint(self.mouse_pos):
+            return True
+        else:
+            return False
+
+    def getPress(self, event, button) -> bool:
         """
         button variable contained 0 (for left button), 1 (for right button)
         :param self:
@@ -54,7 +62,7 @@ class Button:
                 else:
                     return False
 
-    def on_release(self, event, button):
+    def getRelease(self, event, button):
         """
         button variable contained 0 (for left button), 1 (for right button)
         :param event:
@@ -68,3 +76,11 @@ class Button:
                     return True
                 else:
                     return False
+
+    def onTouch(self, event):
+        if self.getTouch(event):
+            try:
+                self.on_touch_action()
+            except Exception as bug:
+             print(bug)
+             print("No action to run")
