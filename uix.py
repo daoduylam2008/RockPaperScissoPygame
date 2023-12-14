414aed31c043563d6f5a3b40df4f5ab50473f73b
daoduylam2008 <96049665+daoduylam2008@users.noreply.github.com>
2023-12-14 19:21:58 +0700

    Update uix.py

--- uix.py
+++ uix.py
@@ -11,25 +11,60 @@ class Button:
                  color=(180, 180, 180),
                  press_color=(100, 100, 100),
                  on_touch_color=(150, 150, 150),
+                 text_color=(0, 0, 0)
                  ):
         self.surface = surface
         self.rect = pygame.Rect(rect[0], rect[1], rect[2], rect[3])
         self.mouse_pos = mouse_pos
         self.event = event
-        self.text = text
+        self.text = self._textForButton(text)
         self.color = color
         self.press_color = press_color
         self.on_touch_color = on_touch_color
+        self.text_color = text_color
 
-    def on_touch(self, event):
+    def create(self):
+        pygame.draw.rect(self.surface, self.color)
+
+    def on_touch(self, event) -> bool:
         self.event = event
+        if self.rect.collidepoint(self.mouse_pos):
+            return True
+        else:
+            return False
 
-    def on_press(self, event):
+    def _textForButton(self, text):
+        font = pygame.font.Font("times new roman", 15)
+        txt = font.render(text, True, self.text_color)
+        return txt
+
+    def on_press(self, event, button) -> bool:
+        """
+        button variable contained 0 (for left button), 1 (for right button)
+        :param self:
+        :param event:
+        :param button:
+        :return:
+        """
         self.event = event
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
+            if event.button == button:
                 if self.rect.collidepoint(self.mouse_pos):
-                    pass
+                    return True
+                else:
+                    return False
 
-    def on_release(self, event):
+    def on_release(self, event, button):
+        """
+        button variable contained 0 (for left button), 1 (for right button)
+        :param event:
+        :param button:
+        :return:
+        """
         self.event = event
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == button:
+                if self.rect.collidepoint(self.mouse_pos):
+                    return True
+                else:
+                    return False
