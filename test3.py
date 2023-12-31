c004bb951ac4a394dae5047abc264bb8490f37e0
Minh Khoi Pham <phamminhkhoi10122008@gmail.com>
2023-12-31 11:43:34 +0700

    finish test 3=) pp ae

--- test3.py
+++ test3.py
@@ -1,12 +1,9 @@
 import pandas as pd
-import numpy as np
-from sklearn.impute import SimpleImputer
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 import random
 import csv
-import time
 
 
 class Computer:
@@ -37,47 +34,56 @@ class Computer:
                 return self.computer_choices[0]
 
     def hard(self):
-        player_choice_dict = {1: 'Rock', 2: 'Paper', 3: 'Scissors'}
-        our_state_dict = {4: 'Win', 5: 'Lose', 6: 'Draw'}
-        fields = ['player_choice', 'our_choice', 'our_state']
-        rows = []
-        our_choice = ''
-        for i in range(len(self.history) - 1):
-            if our_state_dict[self.our_state[i]] == 'Draw':
-                our_choice = player_choice_dict[self.history[i]]
-            elif our_state_dict[self.our_state[i]] == 'Win':
-                match player_choice_dict[self.history[i]]:
-                    case 'Rock':
-                        our_choice = 'Paper'
-                    case 'Paper':
-                        our_choice = 'Scissors'
-                    case 'Scissors':
-                        our_choice = 'Rock'
+        if len(self.history) <=2:
+            return random.choice(self.computer_choices)
+        else:
+            player_choice_dict = {1: 'Rock', 2: 'Paper', 3: 'Scissors'}
+            our_state_dict = {4: 'Win', 5: 'Lose', 6: 'Draw'}
+            fields = ['player_choice', 'our_choice', 'our_state']
+            rows = []
+            our_choice = 0
+            for i in range(len(self.history) - 1):
+                if our_state_dict[self.our_state[i]] == 'Draw':
+                    our_choice = self.history[i]
+                elif our_state_dict[self.our_state[i]] == 'Win':
+                    match player_choice_dict[self.history[i]]:
+                        case 'Rock':
+                            our_choice = 2
+                        case 'Paper':
+                            our_choice = 3
+                        case 'Scissors':
+                            our_choice = 1
+                else:
+                    match player_choice_dict[self.history[i]]:
+                        case 'Rock':
+                            our_choice = 3
+                        case 'Paper':
+                            our_choice = 1
+                        case 'Scissors':
+                            our_choice = 2
+                rows.append([self.history[i], our_choice, self.our_state[i]])
+
+            with open('data/temp.csv', 'w') as csv_file:
+                csvwriter = csv.writer(csv_file)
+                csvwriter.writerow(fields)
+                csvwriter.writerows(rows)
+
+            df = pd.read_csv('data/temp.csv')
+            features = ['player_choice', 'our_state']
+            X = df.dropna(axis=0)[features].values
+            y = df.dropna(axis=0)['our_choice']
+            X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=0)
+
+            dt_model = DecisionTreeClassifier(random_state=1)
+            dt_model.fit(X_train, y_train)
+            dt_model_score = dt_model.score(X_test,y_test)
+
+            rf_model = RandomForestClassifier(random_state=1)
+            rf_model.fit(X_train,y_train)
+            rf_model_score = rf_model.score(X_test,y_test)
+
+            if rf_model_score>=dt_model_score:
+                return player_choice_dict[(rf_model.predict([[self.history[len(self.history) - 1], 4]])[0])]
             else:
-                match player_choice_dict[self.history[i]]:
-                    case 'Rock':
-                        our_choice = 'Scissors'
-                    case 'Paper':
-                        our_choice = 'Rock'
-                    case 'Scissors':
-                        our_choice = 'Paper'
-            rows.append([player_choice_dict[self.history[i]], our_choice, our_state_dict[self.our_state[i]]])
-        rows.append([player_choice_dict[self.history[len(self.history) - 1]], None, 'Win'])
-        with open('data/temp.csv', 'w') as csv_file:
-            csvwriter = csv.writer(csv_file)
-            csvwriter.writerow(fields)
-            csvwriter.writerows(rows)
-        df = pd.read_csv('data/temp.csv')
-        X = df.iloc[:, :2].values
-        y = df.iloc[:, 2].values
-        ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0, 1])], remainder='passthrough')
-        X = ct.fit_transform(X)
-        le = LabelEncoder()
-        y = le.fit_transform(y)
-        imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
+                return player_choice_dict[(dt_model.predict([[self.history[len(self.history) - 1], 4]])[0])]
 
-
-start = time.time()
-a = Computer('rock').hard()
-print(a)
-print(time.time() - start)
